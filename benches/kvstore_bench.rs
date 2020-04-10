e8f25b5747cc20236a7bfe2110c01c9c9d67a68e
CephalonRho <CephalonRho@gmail.com>
2020-04-10 16:11:19 +0200

    Fix benchmark
    This also adds the cost of creating a new tokio runtime for each iteration to the results. Could probably be avoided by using iter_custom or iter_batched, but this should provide enough consistency to still be useful already.

--- benches/kvstore_bench.rs
+++ benches/kvstore_bench.rs
@@ -1,41 +1,54 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use std::future::Future;
 
-use lucid::kvstore::{Encryption, MemoryStore};
+use criterion::{criterion_group, criterion_main, Criterion};
+use tokio::runtime::Runtime;
+
+use lucid::kvstore::{Encryption, MemoryStore, Store};
 
 const CIPHER: &str = "123456789012345678901234123456789012345678901234";
 
 const DATA: [u8; 1000] = [42u8; 1000];
 
+fn run_async<F>(f: F) -> F::Output
+where
+    F: Future,
+{
+    let mut rt = Runtime::new().unwrap();
+    rt.block_on(f)
+}
+
 fn set_1_kb_data(c: &mut Criterion) {
     let kv = MemoryStore::new(Some(Encryption::serpent(hex::decode(CIPHER).unwrap())));
 
     c.bench_function("Set 1KB", |b| {
-        b.iter(|| kv.set("bench_one".to_string(), DATA.to_vec()))
+        b.iter(|| run_async(kv.set("bench_one".to_string(), DATA.to_vec())))
     });
 }
 fn get_1_kb_data(c: &mut Criterion) {
     let kv = MemoryStore::new(Some(Encryption::serpent(hex::decode(CIPHER).unwrap())));
 
     let k = String::from("bench_one");
-    kv.set(k.clone(), DATA.to_vec());
+    run_async(kv.set(k.clone(), DATA.to_vec()));
 
-    c.bench_function("Get 1KB", |b| b.iter(|| kv.get(k.clone())));
+    c.bench_function("Get 1KB", |b| b.iter(|| run_async(kv.get(k.clone()))));
 }
 
 fn set_1_kb_data_without_encryption(c: &mut Criterion) {
     let kv = MemoryStore::new(None);
 
     c.bench_function("Set 1KB (w/o encrytion)", |b| {
-        b.iter(|| kv.set("bench_one".to_string(), DATA.to_vec()))
+        b.iter(|| run_async(kv.set("bench_one".to_string(), DATA.to_vec())))
     });
 }
 fn get_1_kb_data_without_encryption(c: &mut Criterion) {
     let kv = MemoryStore::new(None);
 
     let k = String::from("bench_one");
-    kv.set(k.clone(), DATA.to_vec());
+    run_async(kv.set(k.clone(), DATA.to_vec()));
 
-    c.bench_function("Get 1KB (w/o encryption)", |b| b.iter(|| kv.get(k.clone())));
+    c.bench_function("Get 1KB (w/o encryption)", |b| {
+        b.iter(|| run_async(kv.get(k.clone())))
+    });
 }
 
 criterion_group!(
